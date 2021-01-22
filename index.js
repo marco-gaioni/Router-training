a234ee8c73f352615a9363b3e8db41e9386eec6c
marco-gaioni <gaionimarco2@gmail.com>
2021-01-22 10:36:12 +0100

    modified

--- index.js
+++ index.js
@@ -1,18 +1,21 @@
 const express = require("express")
 const fetch = require("node-fetch")
-const {getPosts} = require("./core/posts")
 const {getComments} = require("./core/comments")
+const {getPosts} = require("./core/posts")
+const port = 8080
 const app = new express()
-const port = 3000
+
+
 
 app.get("/", async (req,res) => {
-    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json()).then((json) => json)
+    const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json()).then((json) => json).catch((err) => console.log(err))
     res.send(getPosts(posts))
 })
 
+
 app.get("/:id", async (req,res) => {
-    const comments = await fetch(`https://jsonplaceholder.typicode.com/post/${req.params.id}/comments`).then((response) => response.json()).then((json) => json)
+    const comments = await fetch(`https://jsonplaceholder.typicode.com/post/${req.params.id}/comments`).then((response) => response.json()).then((json) => json).catch((err) => console.log(err))
     res.send(getComments(comments))
 })
 
-app.listen(port, () => console.log(`app lstening on port ${port}`))
+app.listen(port, () => console.log(`app listening on port ${port}`))
