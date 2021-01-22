a65858f96fee0c61e7df51086e1a81ee9edda5f9
marco-gaioni <gaionimarco2@gmail.com>
2021-01-22 10:50:39 +0100

    ordering code

--- index.js
+++ index.js
@@ -9,13 +9,45 @@ const app = new express()
 
 app.get("/", async (req,res) => {
     const posts = await fetch('https://jsonplaceholder.typicode.com/posts').then((response) => response.json()).then((json) => json).catch((err) => console.log(err))
-    res.send(getPosts(posts))
+    res.send(` <!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+    
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+                    <title>Posts</title>
+                </head>
+                <body>
+                    ${getPosts(posts)}
+    
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+    
+                </body>
+                </html> `)
 })
 
 
 app.get("/:id", async (req,res) => {
     const comments = await fetch(`https://jsonplaceholder.typicode.com/post/${req.params.id}/comments`).then((response) => response.json()).then((json) => json).catch((err) => console.log(err))
-    res.send(getComments(comments))
+    res.send(`  <!doctype html>
+                <html lang="en">
+                <head>
+                    <meta charset="utf-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+                    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+
+                    <title>Comments</title>
+                </head>
+                <body>
+                    ${getComments(comments)}
+
+                    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js" integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW" crossorigin="anonymous"></script>
+
+                </body>
+                </html> `)
 })
 
 app.listen(port, () => console.log(`app listening on port ${port}`))
